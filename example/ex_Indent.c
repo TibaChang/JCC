92378062fe2bec89f1c4d8be0bf083507f70f4c1
JaredCJR <empire89090@gmail.com>
2016-05-14 19:04:41 +0800

    Add example files.

---
+++ example/ex_Indent.c
@@ -0,0 +1,25 @@
+
+struct PP
+{
+    int a;
+}
+
+void main()
+{
+    int testArr[50];
+    int X;
+    struct PP sample;
+    PP.a = 1000;
+
+    for(X=0;X<100;X++)
+    {
+        if(X == 50)
+        {
+            break;
+        }else
+        {
+            PP.a = 500;
+        }
+    }
+    printf("test \n");
+}
